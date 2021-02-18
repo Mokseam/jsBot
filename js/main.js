54855baccd9cc08977fe0bf6faff6927bf88ec13
Mokseam <48885754+Mokseam@users.noreply.github.com>
2021-02-18 18:07:03 +0300

    шаловливые тигрята

--- js/main.js
+++ js/main.js
@@ -4,39 +4,43 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
-let randomNumber = getRandomInt(1, 100);
-console.log(randomNumber);
-toFindNumber();
 
-function askQuestion () {
-    let result = confirm("Вы ввели неверное значение. Хотите продолжить или выйти?");
-    if (result == true) {
-        toFindNumber();
-    }
-    else if (result == false) {
-        alert("Игра закончена");
-    }
-}
+// function askQuestion () {
+//     let result = confirm("Вы ввели неверное значение. Хотите продолжить или выйти?");
+//     if (result == true) {
+//         toFindNumber();
+//     }
+//     else if (result == false) {
+//         alert("Игра закончена");
+//     }
+// }
 
 function toFindNumber() {
+    let randomNumber = getRandomInt(1, 100);
+    console.log('число, которое я загадал: ' + randomNumber);
     let number = +prompt("Угадай число от 1 до 100");
-    if (number < 1 || number > 100) {
-        askQuestion();
+    let equal = number === randomNumber;
+    if (number < 1 || number > 100 || isNaN(number)) {
+        let result = confirm("Вы ввели неверное значение. Хотите продолжить или выйти?");
+        if(result === true) {
+            toFindNumber();
+        }
+        else if(result === false) {
+            alert("Игра закончена");
+            return
+        }
     }
-    else if (number > randomNumber){
+
+    if (number > randomNumber){
         alert("Загаданное число меньше");
-        askQuestion();
     } 
     else if (number < randomNumber) {
         alert("Загаданное число больше");
-        askQuestion();
     } 
-    else if (number == randomNumber) {
+    else if (equal) {
         confirm("Поздравляю, Вы угадали!");
     }
-    else if (parseInt) {
-        askQuestion();
-    }
-
+    return equal === true ? alert("вы угадали!") : toFindNumber();
 };
 
+toFindNumber();
