66b3c835a8a9b53ebd1739c484d160827f7f46df
Mokseam <48885754+Mokseam@users.noreply.github.com>
2021-02-20 20:44:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,21 +3,14 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+  };
 
-// function askQuestion () {
-//     let result = confirm("Вы ввели неверное значение. Хотите продолжить или выйти?");
-//     if (result == true) {
-//         toFindNumber();
-//     }
-//     else if (result == false) {
-//         alert("Игра закончена");
-//     }
-// }
+  function mainFunc() {
+  let randomNumber = getRandomInt(1, 100);
+  console.log('число, которое я загадал: ' + randomNumber);
+  toFindNumber();
 
-function toFindNumber() {
-    let randomNumber = getRandomInt(1, 100);
-    console.log('число, которое я загадал: ' + randomNumber);
+    function toFindNumber() {
     let number = +prompt("Угадай число от 1 до 100");
     let equal = number === randomNumber;
     if (number < 1 || number > 100 || isNaN(number)) {
@@ -41,6 +34,6 @@ function toFindNumber() {
         confirm("Поздравляю, Вы угадали!");
     }
     return equal === true ? alert("Спасибо, что играли в меня") : toFindNumber();
-};
+};};
 
-toFindNumber();
+mainFunc();
